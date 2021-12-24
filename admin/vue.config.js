c5681dff665ae916022a3582ca7838d9d40c2847
jason <>
2021-12-24 17:34:04 +0800

    重构css变量系统

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
     css: {
         loaderOptions: {
             sass: {
-                prependData: `@import "@/styles/variables.scss";`,
+                prependData: '@use "@/styles/variables.scss" as *;',
             }
         }
     },
