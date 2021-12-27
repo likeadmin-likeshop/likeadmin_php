a2902b095e329d0d8229e78283dc33445388593f
jason <>
2021-12-27 18:01:51 +0800

    添加弹窗组件，全局样式变量

---
+++ admin/src/components/popup/index.vue
@@ -0,0 +1,141 @@
+
+<template>
+    <div class="dialog">
+        <div class="dialog__trigger" @click="open">
+            <!-- 触发弹窗 -->
+            <slot name="trigger"></slot>
+        </div>
+        <el-dialog
+            coustom-class="dialog__content"
+            v-model="visible"
+            :width="width"
+            :modal-append-to-body="true"
+            :append-to-body="true"
+            :close-on-click-modal="clickModalClose"
+        >
+            <!-- 弹窗内容 -->
+            <template v-if="title" #title>
+                {{ title }}
+            </template>
+            <template v-else #title>
+                <div class="flex flex-cross-center">
+                    <el-icon :size="25" :color="$variables.color_warning"
+                        ><warning-filled
+                    /></el-icon>
+                    <span class="m-l-6">温馨提示</span>
+                </div>
+            </template>
+
+            <!-- 自定义内容 -->
+            <slot>
+                {{ content }}
+            </slot>
+            <!-- 底部弹窗页脚 -->
+            <template #footer>
+                <div class="dialog-footer">
+                    <el-button
+                        size="small"
+                        v-if="cancelButtonText"
+                        @click="handleEvent('cancel')"
+                    >
+                        {{ cancelButtonText }}
+                    </el-button>
+                    <el-button
+                        size="small"
+                        v-if="confirmButtonText"
+                        @click="handleEvent('confirm')"
+                        type="primary"
+                    >
+                        {{ confirmButtonText }}
+                    </el-button>
+                </div>
+            </template>
+        </el-dialog>
+    </div>
+</template>
+
+
+<script>
+import { provide, ref } from 'vue'
+export default {
+    components: {},
+    emits: ['confirm', 'cancel'],
+    props: {
+        title: {
+            // 弹窗标题
+            type: String,
+            default: '',
+        },
+        content: {
+            // 弹窗内容
+            type: String,
+            default: '确认要删除？',
+        },
+        confirmButtonText: {
+            // 确认按钮内容
+            type: [String, Boolean],
+            default: '确认',
+        },
+        cancelButtonText: {
+            // 取消按钮内容
+            type: [String, Boolean],
+            default: '取消',
+        },
+        width: {
+            // 弹窗的宽度
+            type: String,
+            default: '400px',
+        },
+        disabled: {
+            // 是否禁用
+            type: Boolean,
+            default: false,
+        },
+        async: {
+            // 是否开启异步关闭
+            type: Boolean,
+            default: false,
+        },
+        clickModalClose: {
+            // 点击遮罩层关闭对话窗口
+            type: Boolean,
+            default: true,
+        },
+    },
+    setup(props, { emit }) {
+        const visible = ref(false)
+
+        const handleEvent = (type) => {
+            emit(type)
+            if (!props.async || type === 'cancel') close()
+        }
+
+        const close = () => {
+            visible.value = false
+        }
+        const open = () => {
+            if (props.disabled) return
+            visible.value = true
+        }
+        return {
+            visible,
+            handleEvent,
+            close,
+            open,
+        }
+    },
+}
+</script>
+
+<style scoped lang="scss">
+.dialog-body {
+    white-space: pre-line;
+}
+
+.title-icon {
+    font-size: 24px;
+    color: $color-warning;
+    margin-right: 5px;
+}
+</style>
+
--- admin/src/main.js
+++ admin/src/main.js
@@ -7,10 +7,11 @@ import useElement from './plugins/element'
 import useVueEcharts from './plugins/vue-echarts'
 import cache from './utils/cache'
 import { storeKey } from './config/app'
+import vars from '@/styles/export.scss'
 // 缓存配置
 cache.setKey(storeKey)
 const app = createApp(App)
-
+app.config.globalProperties.$variables = vars
 // element
 useElement(app)
 // vue-echarts
