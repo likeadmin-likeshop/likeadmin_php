749cf97921002fba309ea542dfb9ed24f42d7d94
洪树泽 <2393354236@qq.com>
2022-04-11 17:30:01 +0800

    打包

---
+++ server/public/admin/assets/index.2eda8080.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./index.d388c3ea.js";import{d as s,b as r,c as _,e,l as a}from"./vendor.7e827f17.js";const n=s({}),c={class:"footer-wrap"},d={class:"footer-content"},i={class:"flex flex-center"};function l(o,p,f,v,u,m){return r(),_("div",c,[e("div",d,[e("div",i,[a(o.$slots,"default",{},void 0,!0)])])])}var h=t(n,[["render",l],["__scopeId","data-v-1f623489"]]);export{h as F};
