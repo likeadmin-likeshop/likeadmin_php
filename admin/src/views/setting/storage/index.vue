ac0336967c9168bad66f488a4c3e702ffb214d54
洪树泽 <2393354236@qq.com>
2022-04-21 14:19:19 +0800

    重新打包

--- admin/src/views/setting/storage/index.vue
+++ admin/src/views/setting/storage/index.vue
@@ -2,7 +2,7 @@
 <template>
 	<div class="storage">
 		<el-card shadow="never">
-			<el-alert class="xxl" title="温馨提示：1.切换存储方式后，需要将资源文件传输至新的存储端；2.请勿随意切换存储方式，可能导致图片无法查看。" type="primary"
+			<el-alert class="xxl" title="温馨提示：1.切换存储方式后，需要将资源文件传输至新的存储端；2.请勿随意切换存储方式，可能导致图片无法查看" type="primary"
 				:closable="false" show-icon>
 			</el-alert>
 		</el-card>
