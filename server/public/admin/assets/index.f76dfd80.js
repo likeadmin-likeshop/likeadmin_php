6ee86d363b3c33727477ee2727ca23cbb03ebe46
樹澤 <email@sample.com>
2022-05-30 16:44:53 +0800

    后台打包

--- server/public/admin/assets/index.f76dfd80.js
+++
@@ -1,1 +0,0 @@
-import{d as o,_ as s,f as a,g as _,h as e,aM as n}from"./index.2adab02f.js";const r=o({}),c={class:"footer-wrap"},d={class:"footer-content"},f={class:"flex flex-center"};function i(t,l,p,v,u,x){return a(),_("div",c,[e("div",d,[e("div",f,[n(t.$slots,"default",{},void 0,!0)])])])}var m=s(r,[["render",i],["__scopeId","data-v-6091f4bf"]]);export{m as F};
