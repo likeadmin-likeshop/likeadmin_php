9883d43616de15b06762047b61b6cb817022df5f
mofung1 <2279254178@qq.com>
2022-06-24 16:10:47 +0800

    调整表格列表分页

--- server/app/adminapi/lists/tools/GenerateTableLists.php
+++ server/app/adminapi/lists/tools/GenerateTableLists.php
@@ -55,6 +55,7 @@ class GenerateTableLists extends BaseAdminDataLists implements ListsSearchInterf
         return GenerateTable::where($this->searchWhere)
             ->order(['id' => 'desc'])
             ->append(['template_type_desc'])
+            ->limit($this->limitOffset, $this->limitLength)
             ->select()
             ->toArray();
     }
