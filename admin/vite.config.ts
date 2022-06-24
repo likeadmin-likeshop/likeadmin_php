f2c6fb17331c62c77fe4d2562cbeba1bd13a7022
Jason <5340635+wen-jason@user.noreply.gitee.com>
2022-06-24 15:16:08 +0800

    添加自动引入组件插件

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), AutoImport(), Components()],
   // 引入第三方的配置
   base: '/admin/',
   resolve: {
