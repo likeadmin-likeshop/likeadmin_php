d0e4469add9621fa27af691ddaea7b9b9d01d9a7
Jason <5340635+wen-jason@user.noreply.gitee.com>
2022-07-07 16:39:25 +0800

    新增代码高亮

--- admin/src/views/dev_tools/components/code-preview.vue
+++ admin/src/views/dev_tools/components/code-preview.vue
@@ -10,7 +10,7 @@
                 >
                     <div class="flex">
                         <el-scrollbar class="flex-1" style="height:70vh;">
-                            <pre>{{ item.content }}</pre>
+                            <pre v-html="highlightedCode(item.content, item.name)"></pre>
                         </el-scrollbar>
                         <div>
                             <el-button
@@ -31,6 +31,18 @@ import { CopyDocument } from '@element-plus/icons-vue'
 import { ElMessage } from 'element-plus'
 import { computed, reactive, ref, shallowRef, watch } from 'vue'
 import useClipboard from 'vue-clipboard3'
+import hljs from 'highlight.js/lib/core'
+import javascript from 'highlight.js/lib/languages/javascript'
+import xml from 'highlight.js/lib/languages/xml'
+import ts from 'highlight.js/lib/languages/typescript'
+import php from 'highlight.js/lib/languages/php'
+hljs.registerLanguage("php", php)
+hljs.registerLanguage("ts", ts);
+hljs.registerLanguage("html", xml);
+hljs.registerLanguage("vue", xml);
+hljs.registerLanguage("javascript", javascript)
+import "highlight.js/styles/github.css"
+
 const props = defineProps<{
     modelValue: boolean
     code: any[]
@@ -61,10 +73,13 @@ const show = computed<boolean>({
     }
 })
 
+
+const highlightedCode = (code: string, key: string) => {
+    const index = key.indexOf(".")
+    var language = key.slice(index + 1)
+    const result = hljs.highlight(language, code, true)
+    return result.value || '&nbsp;'
+}
+
 </script>
 
-<style lang="scss" scoped>
-.content {
-    white-space: pre-wrap;
-}
-</style>
