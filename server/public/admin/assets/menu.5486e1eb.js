ebdfc7e2243bc7ee896346b31f571ee374dc4a91
Jason <5340635+wen-jason@user.noreply.gitee.com>
2022-07-28 14:39:57 +0800

    打包

--- server/public/admin/assets/menu.5486e1eb.js
+++
@@ -1,1 +0,0 @@
-import{bE as e}from"./index.e2e7edc0.js";function n(t){return e.get("/auth.menu/lists",t)}function s(t){return e.post("/auth.menu/add",t)}function r(t){return e.post("/auth.menu/edit",t)}function a(t){return e.post("/auth.menu/delete",t)}export{r as a,s as b,a as c,n as m};
