0ffe8bc5e2dcb384691d29d4153e5264ad6e0056
Jason <5340635+wen-jason@user.noreply.gitee.com>
2022-08-04 12:14:41 +0800

    打包

---
+++ server/public/admin/assets/post.63818fab.js
@@ -0,0 +1,1 @@
+import{r as s}from"./index.e62a9b83.js";function o(t){return s.get("dept.jobs/lists",t)}function r(t){return s.post("/dept.jobs/add",t)}function n(t){return s.post("/dept.jobs/edit",t)}function d(t){return s.post("/dept.jobs/delete",t)}export{r as a,d as b,o as c,n as j};
