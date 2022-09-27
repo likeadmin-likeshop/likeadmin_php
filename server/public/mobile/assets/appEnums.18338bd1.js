cde28c38a25fbb804b2c47f57abd03af6a4fbfcf
Jason <5340635+wen-jason@user.noreply.gitee.com>
2022-09-27 09:32:17 +0800

    h5打包

---
+++ server/public/mobile/assets/appEnums.18338bd1.js
@@ -0,0 +1,1 @@
+var I=(I=>(I[I.MP_WEIXIN=1]="MP_WEIXIN",I[I.OA_WEIXIN=2]="OA_WEIXIN",I[I.H5=3]="H5",I[I.IOS=5]="IOS",I[I.ANDROID=6]="ANDROID",I))(I||{}),N=(I=>(I.LOGIN="YZMDL",I.BIND_MOBILE="BDSJHM",I.CHANGE_MOBILE="BGSJHM",I.FIND_PASSWORD="ZHDLMM",I))(N||{}),E=(I=>(I.NONE="",I.AVATAR="avatar",I.USERNAME="account",I.NICKNAME="nickname",I.SEX="sex",I))(E||{});export{I as C,E as F,N as S};
