ff6f7d9d9ee90c36aff176864b99482b54440e08
Jason <5340635+wen-jason@user.noreply.gitee.com>
2022-09-28 18:09:05 +0800

    协议标题问题

--- app/src/pages/agreement/agreement.vue
+++ app/src/pages/agreement/agreement.vue
@@ -5,9 +5,8 @@
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from 'vue'
+import { ref } from 'vue'
 import { onLoad } from '@dcloudio/uni-app'
-import { AgreementEnum } from '@/enums/agreementEnums'
 import { getPolicy } from '@/api/app'
 
 let agreementType = ref('') // 协议类型
@@ -15,12 +14,9 @@ const agreementContent = ref('') // 协议内容
 
 const getData = async (type) => {
     const res = await getPolicy({ type })
-    console.log(res, 'res')
-
     agreementContent.value = res.content
-
     uni.setNavigationBarTitle({
-        title: res.name
+        title: String(res.title)
     })
 }
 
