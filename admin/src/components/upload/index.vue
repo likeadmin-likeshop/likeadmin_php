e428324719f017f0f1d4b9c27b96405ab104e3ae
Jason <5340635+wen-jason@user.noreply.gitee.com>
2022-10-19 17:51:45 +0800

    修复图片上传上传文件类型bug

--- admin/src/components/upload/index.vue
+++ admin/src/components/upload/index.vue
@@ -121,7 +121,7 @@ export default defineComponent({
         const getAccept = computed(() => {
             switch (props.type) {
                 case 'image':
-                    return '.jpj,.png,.gif,.jpeg'
+                    return '.jpg,.png,.gif,.jpeg'
                 case 'video':
                     return '.wmv,.avi,.mpg,.mpeg,.3gp,.mov,.mp4,.flv,.rmvb,.mkv'
                 default:
