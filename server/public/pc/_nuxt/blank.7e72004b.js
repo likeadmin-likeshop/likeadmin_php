0a5cf25bbc8a4c6c9266f32dabecc719b77496f7
Jason <5340635+wen-jason@user.noreply.gitee.com>
2022-10-28 09:33:36 +0800

    打包

---
+++ server/public/pc/_nuxt/blank.7e72004b.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper.a1a6add7.js";import{o as t,f as s,W as n}from"./entry.0685df65.js";const r={},a={class:"layout-blank"};function c(e,l){return t(),s("section",a,[n(e.$slots,"default")])}const d=o(r,[["render",c]]);export{d as default};
