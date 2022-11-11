62da727c7cf27983801441536f18ae61247bcdab
Jason <5340635+wen-jason@user.noreply.gitee.com>
2022-11-11 09:29:15 +0800

    修改pc打包后复制文件的脚本，添加端口号配置

--- pc/scripts/build.mjs
+++ pc/scripts/build.mjs
@@ -1,11 +1,28 @@
 import path from 'path'
 import fsExtra from 'fs-extra'
+import dotenv from 'dotenv'
 const { existsSync, remove, copy } = fsExtra
 const cwd = process.cwd()
-//打包发布路径，谨慎改动
+dotenv.config()
+
+const isSSR = Boolean(process.env.NUXT_SSR)
+//打包发布路径，可能会覆盖重要文件，请谨慎改动
 const releaseRelativePath = '../server/public/pc'
-const distPath = path.resolve(cwd, '.output/public')
+const distRelativePath = isSSR ? '' : './.output/public'
+const distPath = path.resolve(cwd, distRelativePath)
+const distSSRFilter = ['.output', 'public', 'static', 'package.json']
+const distNotSSRFilter = ['']
+const distFilter = joinPath(isSSR ? distSSRFilter : distNotSSRFilter)
+
+function joinPath(target, res = []) {
+    target.forEach((src) => {
+        res.push(path.join(distPath, src))
+    })
+    return res
+}
+
 const releasePath = path.resolve(cwd, releaseRelativePath)
+
 async function build() {
     if (existsSync(releasePath)) {
         await remove(releasePath)
@@ -21,13 +38,23 @@ async function build() {
 
 function copyFile(sourceDir, targetDir) {
     return new Promise((resolve, reject) => {
-        copy(sourceDir, targetDir, (err) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve()
+        copy(
+            sourceDir,
+            targetDir,
+            {
+                filter(src) {
+                    if (src === distPath) return true
+                    return distFilter.some((item) => src.includes(item))
+                }
+            },
+            (err) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve()
+                }
             }
-        })
+        )
     })
 }
 
