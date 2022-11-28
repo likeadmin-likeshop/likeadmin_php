686f1751ab147434dd7f277ac604acc26936aad1
xinjie <>
2022-11-28 15:31:26 +0800

    后台打包

---
+++ server/public/admin/assets/consumer.ca27c1ca.js
@@ -0,0 +1,1 @@
+import{r}from"./index.2267704d.js";function u(e){return r.get({url:"/user.user/lists",params:e},{ignoreCancelToken:!0})}function s(e){return r.get({url:"/user.user/detail",params:e})}function n(e){return r.post({url:"/user.user/edit",params:e})}export{u as a,s as g,n as u};
