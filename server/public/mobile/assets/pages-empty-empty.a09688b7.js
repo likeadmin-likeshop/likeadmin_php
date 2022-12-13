1620b96c4e646aa417f8c2b6ec89a4df286802b2
Jason <5340635+wen-jason@user.noreply.gitee.com>
2022-12-13 10:57:51 +0800

    打包h5

---
+++ server/public/mobile/assets/pages-empty-empty.a09688b7.js
@@ -0,0 +1,1 @@
+import{o as r,e}from"./index.7775237d.js";import{_ as o}from"./plugin-vue_export-helper.21dcd24c.js";var d=o({},[["render",function(o,d){return r(),e("div")}]]);export{d as default};
