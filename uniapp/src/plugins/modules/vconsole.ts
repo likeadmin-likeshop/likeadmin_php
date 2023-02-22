5d126dff447e235378be7447102c5f6ef7fc3579
Jason <5340635+wen-jason@user.noreply.gitee.com>
2023-02-22 19:00:41 +0800

    删除 /

--- uniapp/src/plugins/modules/vconsole.ts
+++ uniapp/src/plugins/modules/vconsole.ts
@@ -1,6 +1,6 @@
 // url中携带参数vconsole == vconsoleMd5时打开Vconsole，可以用于手机浏览器中调试
 // 如/mobile/pages/user/user?vconsole=47b1e3a9d33e6064e58cc4796c708447，此时在个人中心页面打开vconsole
-/
+
 const vconsoleMd5 = '47b1e3a9d33e6064e58cc4796c708447'
 export default async () => {
     // #ifdef H5
