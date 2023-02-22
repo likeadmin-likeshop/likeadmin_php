563d63de2c6b07c997c3f08ffc18053b3369dd05
Jason <5340635+wen-jason@user.noreply.gitee.com>
2023-02-22 18:55:26 +0800

    将vconsole字符串md5化

--- uniapp/src/plugins/modules/vconsole.ts
+++ uniapp/src/plugins/modules/vconsole.ts
@@ -1,13 +1,13 @@
-// url中携带参数vconsole== vconsoleKey时打开Vconsole，可以用于手机浏览器中调试
-// 如/mobile/pages/user/user?vconsole=vconsole2022，此时在个人中心页面打开vconsole
-
-const vconsoleKey = 'vconsole2022'
+// url中携带参数vconsole == vconsoleMd5时打开Vconsole，可以用于手机浏览器中调试
+// 如/mobile/pages/user/user?vconsole=47b1e3a9d33e6064e58cc4796c708447，此时在个人中心页面打开vconsole
+/
+const vconsoleMd5 = '47b1e3a9d33e6064e58cc4796c708447'
 export default async () => {
     // #ifdef H5
     const url = new URL(location.href)
     const searchParams = new URLSearchParams(url.search)
     const vconsole = searchParams.get('vconsole')
-    if (vconsole == vconsoleKey) {
+    if (vconsole == vconsoleMd5) {
         const module: any = await import('vconsole')
         const Vconsole = module.default
         const vConsole = new Vconsole()
