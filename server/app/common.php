01b964ff1fc5a0849266f043c29914f52e9c0ec9
陈健乐 <690712575@qq.com>
2023-04-02 08:18:01 +0000

    修复保存文章正文替换照片会意外替换字符串。如字符串在正文里面有一个超链接之类的，则会去除掉这个域名导致正文的字符串缺失文本。并且导致在web手机端上富文本页面出现无法跳转页面
    Signed-off-by: 陈健乐 <690712575@qq.com>

--- server/app/common.php
+++ server/app/common.php
@@ -195,7 +195,8 @@ function download_file($url, $saveDir, $fileName)
 function clear_file_domain($content)
 {
     $fileUrl = FileService::getFileUrl();
-    return str_replace($fileUrl, '/', $content);
+    $pattern = '/<img[^>]*\bsrc=["\']'.preg_quote($fileUrl, '/').'([^"\']+)["\']/i';
+    return preg_replace($pattern, '<img src="$1"', $content);
 }
 
 
