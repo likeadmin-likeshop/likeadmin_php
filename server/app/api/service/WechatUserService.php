c6e66e93747144e13b50a99e58ae32b6157324e8
mofung1 <2279254178@qq.com>
2023-05-18 09:51:16 +0800

    优化用户注册

--- server/app/api/service/WechatUserService.php
+++ server/app/api/service/WechatUserService.php
@@ -150,8 +150,8 @@ class WechatUserService
         $this->user->channel = $this->terminal;
         $this->user->is_new_user = YesNoEnum::YES;
 
-        if (empty($this->nickname)) {
-            $this->user->nickname = '用户' . $this->user->sn;
+        if ($this->terminal != UserTerminalEnum::WECHAT_MMP && !empty($this->nickname)) {
+            $this->user->nickname = $this->nickname;
         }
 
         $this->user->save();
