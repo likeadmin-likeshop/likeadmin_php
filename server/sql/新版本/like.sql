93edc43ff9c226169eb018822678c6c96852b93b
mofung1 <2279254178@qq.com>
2024-02-05 14:44:55 +0800

    调整更新sql

--- server/sql/新版本/like.sql
+++ server/sql/新版本/like.sql
@@ -2,7 +2,7 @@ SET FOREIGN_KEY_CHECKS=0;
 
 ALTER TABLE `la_file` MODIFY COLUMN `name` varchar(255) NOT NULL DEFAULT '' COMMENT '文件名称' AFTER `type`;
 
-ALTER TABLE `la_operation_log` MODIFY COLUMN `url` text NOT NULL COMMENT '访问链接' AFTER `type`;
+ALTER TABLE `la_operation_log` MODIFY COLUMN `url` varchar(600) NOT NULL COMMENT '访问链接' AFTER `type`;
 
 ALTER TABLE `la_user` MODIFY COLUMN `login_ip` varchar(200) NOT NULL DEFAULT '' COMMENT '最后登录IP' AFTER `is_disable`;
 
