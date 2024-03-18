a5cfa582889246d3d8a3d5638311c37db9956f4c
linjinyuan <2841541624@qq.com>
2024-03-18 17:46:00 +0800

    publish:【移动端】打包测试

---
+++ server/public/mobile/assets/pages-empty-empty.036c65fa.js
@@ -0,0 +1,1 @@
+import{o as r,g as e}from"./index.c081890f.js";import{_ as o}from"./plugin-vue_export-helper.21dcd24c.js";var t=o({},[["render",function(o,t){return r(),e("div")}]]);export{t as default};
--- server/public/mobile/assets/recharge.a66b6e84.js
+++
@@ -1,1 +0,0 @@
-import{a1 as r}from"./index.79c3669d.js";function t(t){return r.post({url:"/recharge/recharge",data:t},{isAuth:!0})}function e(t){return r.get({url:"/recharge/lists",data:t},{isAuth:!0})}function a(){return r.get({url:"/recharge/config"},{isAuth:!0})}export{t as a,e as b,a as r};
