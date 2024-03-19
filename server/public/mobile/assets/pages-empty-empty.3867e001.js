22f50b334c64a711690d10039c2540d9c962a3ea
linjinyuan <2841541624@qq.com>
2024-03-19 16:03:45 +0800

    publish:【移动端】打包测试

---
+++ server/public/mobile/assets/pages-empty-empty.3867e001.js
@@ -0,0 +1,1 @@
+import{o as r,g as e}from"./index.bf783827.js";import{_ as o}from"./plugin-vue_export-helper.21dcd24c.js";var t=o({},[["render",function(o,t){return r(),e("div")}]]);export{t as default};
--- server/public/mobile/assets/pages-empty-empty.4cc022bf.js
+++
@@ -1,1 +0,0 @@
-import{o as r,g as e}from"./index.3fa91c6a.js";import{_ as a}from"./plugin-vue_export-helper.21dcd24c.js";var o=a({},[["render",function(a,o){return r(),e("div")}]]);export{o as default};
