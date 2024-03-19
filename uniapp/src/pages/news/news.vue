d18c8171fa612054d5f60282d5b32afdc2886c07
linjinyuan <2841541624@qq.com>
2024-03-19 17:37:09 +0800

    fix:【小程序-导航丢失】https://www.tapd.cn/38064061/bugtrace/bugs/view?bug_id=1138064061001010512

--- uniapp/src/pages/news/news.vue
+++ uniapp/src/pages/news/news.vue
@@ -7,6 +7,23 @@
         />
         <!-- #endif -->
     </page-meta>
+    <!-- #ifndef H5 -->
+    <u-sticky
+        h5-nav-height="0"
+        bg-color="transparent"
+    >
+        <u-navbar
+            :is-back="false"
+            :is-fixed="false"
+            title="资讯"
+            :border-bottom="false"
+            :title-bold="true"
+            :title-color="$theme.navColor"
+            :background="{ background: $theme.navBgColor }"
+        >
+        </u-navbar>
+    </u-sticky>
+    <!-- #endif -->
     <view class="news">
         <!-- 搜索 -->
         <navigator class="news-search px-[24rpx] py-[14rpx] bg-white" url="/pages/search/search">
