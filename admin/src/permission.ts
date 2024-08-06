5d5218cbd493bea4bb86f6c39f3e06c8755ba092
linjinyuan <2841541624@qq.com>
2024-08-06 17:54:49 +0800

    fix:【后台】 修复后台超过3级菜单时使用缓存功能失效的问题

--- admin/src/permission.ts
+++ admin/src/permission.ts
@@ -3,18 +3,54 @@
  */
 
 import NProgress from 'nprogress'
-import router, { findFirstValidRoute } from './router'
+import router, {findFirstValidRoute} from './router'
 import 'nprogress/nprogress.css'
-import { isExternal } from './utils/validate'
+import {isExternal} from './utils/validate'
 import useUserStore from './stores/modules/user'
-import { INDEX_ROUTE, INDEX_ROUTE_NAME } from './router/routes'
-import { PageEnum } from './enums/pageEnum'
+import {INDEX_ROUTE, INDEX_ROUTE_NAME} from './router/routes'
+import {PageEnum} from './enums/pageEnum'
 import useTabsStore from './stores/modules/multipleTabs'
-import { clearAuthInfo } from './utils/auth'
+import {clearAuthInfo} from './utils/auth'
 import config from './config'
 
+// 动态添加路由-使用递归进行调整-（fix: 修复之前超过3级菜单导致使用keep-alive功能无效问题
+const addRoutesRecursively = (routes: any, parentPath = '') => {
+    try {
+        routes.forEach((route: any) => {
+            // 如果路由是外部链接，则不添加
+            if (isExternal(route.path)) {
+                return;
+            }
+
+            // 拼接父路由路径和当前路由路径
+            const fullPath = parentPath + route.path;
+
+            // 创建路由对象，确保每个路由都有唯一的名称
+            const routerEntry = {
+                ...route,
+                path: fullPath,
+                name: route.name || fullPath.replace(/\//g, '_').replace('_', '') // 替换斜杠为下划线，生成唯一名称
+            };
+
+            // 添加路由
+            if (!route.children) {
+                router.addRoute(INDEX_ROUTE_NAME, routerEntry);
+            } else {
+                router.addRoute(routerEntry);
+            }
+
+            // 递归处理子路由
+            if (route.children && route.children.length > 0) {
+                addRoutesRecursively(route.children, fullPath + '/');
+            }
+        });
+    } catch (e) {
+        console.error('Error adding routes:', e);
+    }
+};
+
 // NProgress配置
-NProgress.configure({ showSpinner: false })
+NProgress.configure({showSpinner: false})
 
 const loginPath = PageEnum.LOGIN
 const defaultPath = PageEnum.INDEX
@@ -34,7 +70,7 @@ router.beforeEach(async (to, from, next) => {
         const hasGetUserInfo = Object.keys(userStore.userInfo).length !== 0
         if (hasGetUserInfo) {
             if (to.path === loginPath) {
-                next({ path: defaultPath })
+                next({path: defaultPath})
             } else {
                 next()
             }
@@ -51,30 +87,20 @@ router.beforeEach(async (to, from, next) => {
                     return
                 }
                 tabsStore.setRouteName(routeName!)
-                INDEX_ROUTE.redirect = { name: routeName }
+                INDEX_ROUTE.redirect = {name: routeName}
 
                 // 动态添加index路由
                 router.addRoute(INDEX_ROUTE)
-                routes.forEach((route: any) => {
-                    // https 则不插入
-                    if (isExternal(route.path)) {
-                        return
-                    }
-                    if (!route.children) {
-                        router.addRoute(INDEX_ROUTE_NAME, route)
-                        return
-                    }
-                    // 动态添加可访问路由表
-                    router.addRoute(route)
-                })
-                next({ ...to, replace: true })
+                // 动态添加其余路由
+                addRoutesRecursively(routes)
+                next({...to, replace: true})
             } catch (err) {
                 clearAuthInfo()
-                next({ path: loginPath, query: { redirect: to.fullPath } })
+                next({path: loginPath, query: {redirect: to.fullPath}})
             }
         }
     } else {
-        next({ path: loginPath, query: { redirect: to.fullPath } })
+        next({path: loginPath, query: {redirect: to.fullPath}})
     }
 })
 
