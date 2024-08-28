ac5e307e975d698afa2b30abb785a808e24f512e
JXDN <2749143951@qq.com>
2024-08-28 18:33:01 +0800

    fix: 代码生成器代码高亮问题修复

--- admin/src/install/plugins/hljs.ts
+++ admin/src/install/plugins/hljs.ts
@@ -3,6 +3,7 @@ import 'highlight.js/styles/github.css'
 import hljsVuePlugin from '@highlightjs/vue-plugin'
 import hljs from 'highlight.js'
 import type { App } from 'vue'
+console.log(hljs)
 
 export default (app: App<Element>) => {
     app.use(hljsVuePlugin)
