c28ba0b073058d630792da9d1ed861ae48409559
JXDN <2749143951@qq.com>
2024-08-28 18:26:27 +0800

    fix: 后台代码生成器代码高亮失效

--- admin/src/install/plugins/hljs.ts
+++ admin/src/install/plugins/hljs.ts
@@ -1,6 +1,7 @@
 import 'highlight.js/styles/github.css'
 
 import hljsVuePlugin from '@highlightjs/vue-plugin'
+import hljs from 'highlight.js'
 import type { App } from 'vue'
 
 export default (app: App<Element>) => {
