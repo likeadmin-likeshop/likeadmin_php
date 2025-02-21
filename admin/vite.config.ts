dce6ac6e370753a7d0d7b748776925d4060a13df
JXDN <2749143951@qq.com>
2025-02-21 14:21:30 +0800

    feat: 依赖升级，合并ts配置文件

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,3 +1,5 @@
+import * as fs from 'node:fs'
+import * as path from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -10,10 +12,36 @@ import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import vueSetupExtend from 'vite-plugin-vue-setup-extend'
 // import legacyPlugin from '@vitejs/plugin-legacy'
 // https://vitejs.dev/config/
+
+function getElementPlusStylePaths() {
+    const componentsDir = path.resolve(
+        __dirname,
+        'node_modules',
+        'element-plus',
+        'es',
+        'components'
+    )
+    const componentDirs = fs.readdirSync(componentsDir)
+
+    const stylePaths: string[] = []
+
+    // 遍历每个组件文件夹，检查是否有 `style/css` 文件
+    componentDirs.forEach((dir) => {
+        const stylePath = path.join(componentsDir, dir, 'style', 'index.mjs')
+        if (fs.existsSync(stylePath)) {
+            stylePaths.push(`element-plus/es/components/${dir}/style/css`)
+        }
+    })
+
+    return stylePaths
+}
+
 export default defineConfig({
     base: '/admin/',
     server: {
-        host: '0.0.0.0'
+        host: '0.0.0.0',
+        hmr: true,
+        open: true
     },
     plugins: [
         vue(),
@@ -38,20 +66,30 @@ export default defineConfig({
             symbolId: 'local-icon-[dir]-[name]'
         }),
         vueSetupExtend()
-        // legacyPlugin({
-        //     targets: ['defaults', 'IE 11']
-        // })
     ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler'
+            }
+        }
+    },
+    optimizeDeps: {
+        // 确保不重复包含这些依赖
+        include: ['@wangeditor/editor-for-vue', 'vue-echarts', ...getElementPlusStylePaths()]
+    },
     build: {
         rollupOptions: {
-            manualChunks(id) {
-                if (id.includes('node_modules')) {
-                    return id.toString().split('node_modules/')[1].split('/')[0].toString()
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString()
+                    }
                 }
             }
         }
